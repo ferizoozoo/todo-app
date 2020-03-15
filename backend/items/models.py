3b9ddf60afe0220f3a5358a0d4f849943f448bb1
ferizoozoo <farhadzohoorz@gmail.com>
2020-03-15 16:11:43 +0330

    Add the TodoItem model

--- backend/items/models.py
+++ backend/items/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TodoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline_at = models.DateTimeField()
+    active = models.BooleanField()
+    completed = models.BooleanField()
+    priority = models.BooleanField()
