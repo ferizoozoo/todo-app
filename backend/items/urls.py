dbd0ed4a86a459e70e614d95a94ed5fea0d9a77b
ferizoozoo <farhadzohoorz@gmail.com>
2020-03-15 16:14:17 +0330

    Add the proper urls via routers for the TodoItemViewSet viewset

--- backend/items/urls.py
+++ backend/items/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import TodoItemViewSet
+
+router = routers.SimpleRouter()
+router.register(r'todoItems', TodoItemViewSet)
 
 urlpatterns = [
-    
+    path('v1', include(router.urls))
 ]
