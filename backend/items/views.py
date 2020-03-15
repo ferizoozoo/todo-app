34791cc96059cc1d344ad1feb439fd79f0d0a3e1
ferizoozoo <farhadzohoorz@gmail.com>
2020-03-15 16:13:26 +0330

    Add the TodoItemViewSet viewset

--- backend/items/views.py
+++ backend/items/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from .models import TodoItem
+from .serializers import TodoItemSerializer
 
 # Create your views here.
+class TodoItemViewSet(viewsets.ModelViewSet):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
