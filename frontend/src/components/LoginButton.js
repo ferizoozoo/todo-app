b1056e4c7fc9a674a8c5cc67f0051feed06f723e
ferizoozoo <farhadzohoorz@gmail.com>
2020-03-16 12:30:07 +0330

    Add the components folder to the project

---
+++ frontend/src/components/LoginButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoginButton extends React.Component{
+    render(){
+        return (
+            <div>
+                <button src="#" className="">Login</button>
+            </div>
+        )
+    }
+}
